cf692f95ae231fa2a54574f72b115bbfac899c7e
Clemens Oberhauser <clemens.oberhauser@gmail.com>
2026-05-18 18:54:33 +0200

    Change Dlls to native to fix it for canflicts with other libs

--- Fido2Net/Util/Init.cs
+++ Fido2Net/Util/Init.cs
@@ -1,4 +1,8 @@
 ﻿using Fido2Net.Interop;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 
 namespace Fido2Net.Util
@@ -20,7 +24,32 @@ namespace Fido2Net.Util
             }
 
             _called = true;
+
+            // On Windows the native fido2/cbor DLLs live in native\ to avoid a name collision
+            // with the managed Fido2.dll (fido2-net-lib) and CBOR.dll (PeterO.Cbor) assemblies.
+            // Register a resolver so DllImport("fido2") always finds the correct native binary.
+            if (OperatingSystem.IsWindows())
+                RegisterNativeResolver();
+
             Native.fido_init((int)Fido2Settings.Flags);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void RegisterNativeResolver()
+        {
+            var nativeDir = Path.Combine(AppContext.BaseDirectory, "native");
+            if (!Directory.Exists(nativeDir))
+                return;
+
+            NativeLibrary.SetDllImportResolver(typeof(Init).Assembly,
+                (name, _, _) =>
+                {
+                    var path = Path.Combine(nativeDir, name + ".dll");
+                    return File.Exists(path) ? NativeLibrary.Load(path) : IntPtr.Zero;
+                });
         }
 
         #endregion
